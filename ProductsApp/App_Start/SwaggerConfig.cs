8fc6a79980d6262d7b34d67730a531eb6b764d9b
Jason Wu(Aero 15) <wujpkimo@gmail.com>
2018-08-12 13:35:12 +0800

    05 練習多種路由寫法，包含路由限定詞與萬用路由參數

--- ProductsApp/App_Start/SwaggerConfig.cs
+++ ProductsApp/App_Start/SwaggerConfig.cs
@@ -183,7 +183,7 @@ namespace ProductsApp
                         // Use the "DocumentTitle" option to change the Document title.
                         // Very helpful when you have multiple Swagger pages open, to tell them apart.
                         //
-                        //c.DocumentTitle("My Swagger UI");
+                        c.DocumentTitle("My Swagger UI");
 
                         // Use the "InjectStylesheet" option to enrich the UI with one or more additional CSS stylesheets.
                         // The file must be included in your project as an "Embedded Resource", and then the resource's
---
+++ ProductsApp/Controllers/ClientsController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace ProductsApp.Models
+{
+    [RoutePrefix("clients")]
+    public class ClientsController : ApiController
+    {
+        private FabricsEntities db = new FabricsEntities();
+
+        public ClientsController()
+        {
+            db.Configuration.LazyLoadingEnabled = false;
+        }
+
+        [Route("")]
+        // GET: api/Clients
+        public IQueryable<Client> GetClient()
+        {
+            return db.Client.Take(10);
+        }
+
+        // GET: api/Clients/5
+        [Route("{id}", Name = nameof(GetNameById))]
+        [ResponseType(typeof(Client))]
+        public IHttpActionResult GetNameById(int id)
+        {
+            Client client = db.Client.Find(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(client);
+        }
+
+        [Route("{id:int}/orders", Name = nameof(GetClientOrders))]
+        [ResponseType(typeof(Client))]
+        public IHttpActionResult GetClientOrders(int id)
+        {
+            var orders = db.Order.Where(p => p.ClientId == id);
+            return Ok(orders.ToList());
+        }
+
+        [Route("{id:int}/orders/{date:datetime}", Name = nameof(GetClientOrdersByDate1))]
+        [ResponseType(typeof(Client))]
+        public IHttpActionResult GetClientOrdersByDate1(int id, DateTime date)
+        {
+            var next_day = date.AddDays(1);
+            var orders = db.Order.Where(p => p.ClientId == id && p.OrderDate > date && p.OrderDate <= next_day);
+            return Ok(orders.ToList());
+        }
+
+        [Route("{id:int}/orders/{*date:datetime}", Name = nameof(GetClientOrdersByDate2))]
+        [ResponseType(typeof(Client))]
+        public IHttpActionResult GetClientOrdersByDate2(int id, DateTime date)
+        {
+            var next_day = date.AddDays(1);
+            var orders = db.Order.Where(p => p.ClientId == id && p.OrderDate > date && p.OrderDate <= next_day);
+            return Ok(orders.ToList());
+        }
+
+        [Route("{id}")]
+        // PUT: api/Clients/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutClient(int id, Client client)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != client.ClientId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(client).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClientExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        [Route("")]
+        // POST: api/Clients
+        [ResponseType(typeof(Client))]
+        public IHttpActionResult PostClient(Client client)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Client.Add(client);
+            db.SaveChanges();
+
+            return CreatedAtRoute(nameof(GetNameById), new { id = client.ClientId }, client);
+        }
+
+        // DELETE: api/Clients/5
+        [Route("{id}")]
+        [ResponseType(typeof(Client))]
+        public IHttpActionResult DeleteClient(int id)
+        {
+            Client client = db.Client.Find(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            db.Client.Remove(client);
+            db.SaveChanges();
+
+            return Ok(client);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ClientExists(int id)
+        {
+            return db.Client.Count(e => e.ClientId == id) > 0;
+        }
+    }
+}
