8dab76a3fd11b218fc2206b911146bb0b62f417e
Jason Wu(Aero 15) <wujpkimo@gmail.com>
2018-08-12 09:16:41 +0800

    first commit

---
+++ ProductsApp/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using ProductsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProductsApp.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+        };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public Product GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            return product;
+        }
+    }
+}
