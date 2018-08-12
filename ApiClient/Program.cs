b22a29c2036f9196e8651a750891932ad1fe2156
Jason Wu(Aero 15) <wujpkimo@gmail.com>
2018-08-12 12:09:01 +0800

    實作swag studio自動產生c# client(console)

---
+++ ApiClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiClient
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Run();
+
+            Console.ReadLine();
+        }
+
+        public static async Task Run()
+        {
+            var client = new ProductsClient();
+
+            var product = await client.GetProductByIdAsync(1);
+
+            Console.WriteLine(product.ProductName);
+        }
+    }
+}
